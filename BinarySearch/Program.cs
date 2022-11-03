1360cfe2ec25a06c9b62f03bd422f62cc8ae0370
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-11-03 15:31:00 +0200

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static int binarySearch(int[] arr, int lowIndex, int highIndex, int valueToSearch)
+        {
+            if (highIndex >= lowIndex)
+            {
+                int midIndex = lowIndex + (highIndex - lowIndex) / 2;
+
+                if (arr[midIndex] == valueToSearch)
+                    return midIndex;
+
+                if (arr[midIndex] > valueToSearch)
+                    return binarySearch(arr, lowIndex, midIndex - 1, valueToSearch);
+
+                return binarySearch(arr, midIndex + 1, highIndex, valueToSearch);
+            }
+
+            return -1;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 10, 40 };
+            int len = arr.Length;
+            int valueToSearch = 30;
+
+            int result = binarySearch(arr, 0, len - 1, valueToSearch);
+
+            if (result == -1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at index " + result);
+        }
+    }
+}
